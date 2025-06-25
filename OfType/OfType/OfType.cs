f4ea6d1e5486daa6e92dfafc0d882dcb82731f26
Or Sapir <orsapir10@gmail.com>
2025-06-25 18:13:28 +0300

    Implement IOfType Ineterface

---
+++ OfType/OfType/OfType.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace OfType
+{
+    public class OfType : IOfType
+    {
+        public IEnumerable<T> GetOfType<T>(IEnumerable enumerable)
+        {
+            return enumerable.OfType<T>();
+        }
+
+        public IEnumerable<TOutput> GetOfType<TSource, TOutput>(IEnumerable<TSource> enumerable)
+        {
+            return GetOfType<TOutput>(enumerable);
+        }
+
+        public IEnumerable<TBase> OfBase<TBase, TDerived>(IEnumerable<TDerived> derivedItems) where TDerived : TBase
+        {
+            return derivedItems.OfType<TBase>();
+        }
+    }
+}
